a8d92b4397980e11823e391a48fbd557bd9b458d
namikemen <87888839+namikemen@users.noreply.github.com>
2023-03-06 20:57:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Lidy';
+const giftUrl = 'http://nodemy.vn';
+const eventName = "Happy Women's day";
+const titleCard = 'Dear you';
+const contentCard = "On this special day of Women's Day, I want to celebrate and honor the incredible woman that you are. Your intelligence, kindness, and strength never cease to amaze me, and I feel so lucky to have you in my life. You are an inspiration to so many people, including me, and I am constantly in awe of your perseverance and determination. Your achievements are a testament to your hard work and dedication, and I know that you will continue to accomplish amazing things. Thank you for being an amazing girlfriend and for bringing so much joy and happiness into my life. I feel so blessed to have you by my side, and I am excited to see all the incredible things you will accomplish in the future. On this Women's Day, I want you to know how much you are appreciated and loved. Keep shining bright, my love!";
+
+
+const giftImage = 'IMG_0370.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
